6f9e63731060c8dfe4b95a8b1b678b503927978a
fredalex6 <frederik.a.adlandsvik@hotmail.com>
2026-07-13 23:44:45 +0200

    config: additions and cleanups

---
+++ config.py
@@ -0,0 +1,27 @@
+# move indices
+ROCK = 0
+SCISSORS = 1
+PAPIR = 2
+
+MOVE_NAMES = ["stein", "saks", "papir"]
+
+# network memory each turn
+HISTORY_WINDOW = 5
+
+# randomly played rounds before prediction-making
+RANDOM_ROUNDS = 5
+
+# replay buffer
+BUFFER_SIZE = 150
+
+# training passes over the buffer
+EPOCHS_PER_ROUND = 30
+
+LEARNING_RATE = 0.01
+
+
+######################################
+
+# GUI greier
+
+FONT = "Helvetica"
